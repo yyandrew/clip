351675d33d8a4d1ab7ed4fc488a957f92a9693aa
lq201 <qqerqqer@126.com>
2026-05-03 10:13:00 +0800

    Fix error: /usr/lib/x86_64-linux-gnu/gvfs/libgvfscommon.so: undefined symbol: g_variant_builder_init_static

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 # main.py
+import os
 import sys
+
+# 禁用 GVFS/GIO 模块加载，避免不同系统 glib 版本不兼容报错
+os.environ['GIO_MODULE_DIR'] = ''
+
 from PyQt6.QtWidgets import QApplication
 from clip import ClipboardApp
 
